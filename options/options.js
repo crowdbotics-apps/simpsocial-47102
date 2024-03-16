3aa1dfec10c56b6dc00f49d7ad6a72394d9c9951
Hafiz Muhammad Sheharyar <muhammad.sheharyar@tkxel.com>
2024-03-17 00:03:42 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "simpsocial-47102",
   url: "https://simpsocial-47102.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
